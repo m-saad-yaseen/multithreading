98f7e2a4b03b7d55f9999f835cca8dc824230d50
Saad <saad@itconquest.com>
2020-03-11 22:17:35 -0400

    Handling RuntimeException within a misbehaving thread

--- src/main/java/MultiThreading.java
+++ src/main/java/MultiThreading.java
@@ -6,13 +6,20 @@ public class MultiThreading {
             public void run() {
                 System.out.println("I am running in a thread " + Thread.currentThread().getName());
                 System.out.println("Thread priority is: " + Thread.currentThread().getPriority());
+                throw new RuntimeException("Intentional Exception");
             }
         });
-        thread.setName("New Worker Thread");
+        thread.setName("Misbehaving Thread");
         thread.setPriority(Thread.MAX_PRIORITY);
 
         System.out.println("We are in thread: " + Thread.currentThread().getName() + " before starting a new thread.");
         thread.start();
         System.out.println("We are in thread: " + Thread.currentThread().getName() + " after starting a new thread.");
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("Critical Error");
+            }
+        });
     }
 }
